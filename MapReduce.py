b348985bec337fda658719a548e714c2ac114fbb
jrdonzello <jrdonzello@miners.utep.edu>
2020-02-23 18:43:56 -0700

    second committance

--- MapReduce.py
+++ MapReduce.py
@@ -3,20 +3,32 @@ import pymp, time, re
 class MapReduce():
     words = ["hate", "love", "death", "night", "sleep", "time", "henry", "hamlet","you", "my","blood","poison","macbeth","king","heart","honest"]
     files = ["shakespeare1.txt","shakespeare2.txt","shakespeare3.txt","shakespeare4.txt","shakespeare5.txt","shakespeare6.txt","shakespeare7.txt","shakespeare8.txt"]
+
+    def main(self):
+        #total run time
+        start = time.process_time()
+        self.runParallelWC()
+        end = time.process_time() - start
+        print('Total runtime: {}'.format(end))
+
+
     def runParallelWC(self):
         wordCount = pymp.shared.dict()
-        with pymp.Parallel() as p:
+        with pymp.Parallel(2) as p:
             lock = p.lock
             for x in self.words:
                 wordCount[x] = 0
             for item in p.iterate(self.files):
+                start = time.process_time()
                 current_file = open(item, "r")
                 current_file = current_file.read()
                 for word in self.words:
                     count = re.findall(word, current_file)
                     lock.acquire()
-                    wordCount[word] = len(count)
+                    wordCount[word] += len(count)
                     lock.release()
-        print(wordCount)
+                end = time.process_time() - start
+                print('Thread num {} - Current file {} runtime: {}'.format(p.thread_num,item,end))
+        #print(wordCount)
 mapReduce = MapReduce()
-mapReduce.runParallelWC()
+mapReduce.main()
